928bf241a80e3e9683fdc02ea16317af1af30553
Julian <juliankl1509@gmail.com>
2024-08-04 00:03:38 +0200

    Enhance UX on detail view.
    Disable stripe tmp

--- andelsbolig-frontend/main.js
+++ andelsbolig-frontend/main.js
@@ -7,6 +7,7 @@ import {setupRegisterView} from "./register/register.js";
 import {setupLogoutView} from "./logout/logout.js";
 import {setupLoginView} from "./login/login.js";
 import {insertHeader} from "./header/header.js";
+import {displayAdvertisementDetail} from "./advertisement_detail/advertisement_detail.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     insertHeader();
@@ -21,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Call on page load
     fetchAndDisplayAdvertisements();
-    showView('home');
+    // showView('register');
+    // displayAdvertisementDetail('66ad6718d8ed65fe7d1b5624')
 });
 
