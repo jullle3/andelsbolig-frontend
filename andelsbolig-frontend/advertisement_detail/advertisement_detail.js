7890d97d1adef71df9ae43b17fff7ecc2b3aba82
Julian <juliankl1509@gmail.com>
2024-08-03 23:42:22 +0200

    Enhance UX on detail view

--- andelsbolig-frontend/advertisement_detail/advertisement_detail.js
+++ andelsbolig-frontend/advertisement_detail/advertisement_detail.js
@@ -24,11 +24,11 @@ export async function displayAdvertisementDetail(advertisement_id) {
 
 
 <!-- Modal for Viewing Full-Size Image -->
-<div class="modal fade" id="fullImageModal" tabindex="-1" aria-labelledby="fullImageModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl">
+<div class="modal fade" id="fullImageModal" tabindex="1" aria-labelledby="fullImageModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="fullImageModalLabel">Full Size Image</h5>
+<!--                <h5 class="modal-title" id="fullImageModalLabel">Full Size Image</h5>-->
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
